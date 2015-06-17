d3491a7b22975dcfbe31f357a0256c1f432a4452
BrightStarIsNameBetelgeuse <mainyvar@gmail.com>
2015-06-17 16:27:39 +0600

    Change main page, mapping of films list

--- js/controllers/movieController.js
+++ js/controllers/movieController.js
@@ -1,4 +1,10 @@
+var model={
+	items:[
+		{movie:"111",descr:"qqq",actors:"t r t"},
+		{movie:"222",descr:"hhh", actors:"t r t"}
+	]
+};
 var myApp=angular.module("myApp",[]);
 myApp.controller('movieController', function($scope){
-	
+	$scope.list = model;
 });
